0aec2138226c16d1487337dc013ab94fba3571ca
Valeria1100 <valeria.norena1107@hotmail.com>
2025-02-18 19:14:20 -0500

    Add files via upload

---
+++ #EJERCICIO 1.py
@@ -0,0 +1,24 @@
+#EJERCICIO 1
+numero1= 4
+numero2= 8
+resultado= numero1 + numero2
+print("La suma es ", resultado)
+resultado= numero1 - numero2
+print("La resta es ", resultado)
+resultado= numero1 / numero2
+print("La division es ", resultado)
+resultado= numero1 * numero2
+print("La multilicacion es ", resultado)
+resultado= numero1 % numero2
+print("el promedio es ", resultado)
+
+#EJERCICIO 2
+numero3 = 34000
+resultado= numero3*15/100
+print("La prenda vale 34000 y tiene un descuento del 15% ")
+print("la prenda queda en", -(resultado - 34000))
+
+#EJERCICIO 3
+numero4 = 10
+resultado= numero4 +5-3
+print("el numero es ", resultado*2/4)
